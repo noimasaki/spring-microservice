9be545a2e428ae707cf456f68fb5c419519a776d
noimasaki <masaki1108.007@gmail.com>
2023-12-21 00:31:39 +0900

    mod

--- frontend-webapp/src/main/java/com/example/frontendwebapp/frontController.java
+++ frontend-webapp/src/main/java/com/example/frontendwebapp/frontController.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class frontController {
 
-    @GetMapping(value = "/login")
+    @GetMapping("/login")
     public String login(){
         // Thymeleafを利用しているため、記載で`/resources/templates/login.html`をreturnする
         return "login";
+    }
+
+    @GetMapping
+    public String home(){
+        return "home";
     }
 
 }
