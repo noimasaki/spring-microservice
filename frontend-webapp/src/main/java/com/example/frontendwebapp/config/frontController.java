df87532fba5b31b948a06648efbcf73c589abf5c
noimasaki <masaki1108.007@gmail.com>
2023-12-21 01:04:53 +0900

    ok

--- frontend-webapp/src/main/java/com/example/frontendwebapp/config/frontController.java
+++ frontend-webapp/src/main/java/com/example/frontendwebapp/config/frontController.java
@@ -1,4 +1,4 @@
-package com.example.frontendwebapp;
+package com.example.frontendwebapp.config;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
