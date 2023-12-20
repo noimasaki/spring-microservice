7296a518c454a14bf5dfd777e3332b94fc89f123
noimasaki <masaki1108.007@gmail.com>
2023-12-20 23:15:12 +0900

    SpringInit

---
+++ frontend-webapp/src/test/java/com/example/frontendwebapp/FrontendWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontendwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
