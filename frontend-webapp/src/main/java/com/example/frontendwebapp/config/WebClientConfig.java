48587953f3281bffe1cd0b724d10055f9bdd5dfa
noimasaki <masaki1108.007@gmail.com>
2024-01-24 16:54:44 +0900

    webclient

--- frontend-webapp/src/main/java/com/example/frontendwebapp/config/WebClientConfig.java
+++ frontend-webapp/src/main/java/com/example/frontendwebapp/config/WebClientConfig.java
@@ -1,27 +1,29 @@
 package com.example.frontendwebapp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestOperations;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.example.frontendwebapp.app.web.ServiceProperties;
 
 @Configuration
 @ComponentScan("com.example.frontendwebapp.app.web")    //Controllerクラスは別ディレクトリなので読み込んであげる
-public class MvcConfig implements WebMvcConfigurer{
-    
+
+public class WebClientConfig {
     // backendを呼び出すときの基本URIをServicePropertiesから取得する
     // つまり、「/backend/items」へリクエストを送信するときに
     // getDns()メソッドで取得した基本URIを設定して「http://xxxx.com/backend/items」へリクエストを送信する
-    @Autowired
-    ServiceProperties properties;
 
+    @Autowired
+    ServiceProperties serviceProperties;
+    
     @Bean
-    public RestOperations restOperations(RestTemplateBuilder restTemplateBuilder){
-        return restTemplateBuilder.rootUri(properties.getDns()).build();
+    public WebClient webClient(){
+        return WebClient.builder()
+            .baseUrl(serviceProperties.getDns())
+            .build();
     }
+    
 }
--- frontend-webapp/src/main/java/com/example/frontendwebapp/domain/service/frontItemService.java
+++ frontend-webapp/src/main/java/com/example/frontendwebapp/domain/service/frontItemService.java
@@ -1,8 +1,10 @@
 package com.example.frontendwebapp.domain.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class frontItemService {
@@ -13,11 +15,23 @@ public class frontItemService {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    public String getAllItems() {
-        String URL = "http://localhost:8081/backend/items";
-        String items = restTemplate.getForObject(URL, String.class);
+    // public String getAllItems() {
+    //     String URL = "http://localhost:8081/backend/items";
+    //     String items = restTemplate.getForObject(URL, String.class);
 
-        return items;
+    //     return items;
+    // }
+
+    @Autowired
+    WebClient webClient;
+
+    public String getAllItems(){
+        return webClient.get()
+            .uri("/backend/items")
+            .retrieve() // retrieveの後にレスポンスを抽出する方法を記述する
+            .bodyToMono(String.class)   // String型で受け取る
+            .block();   // ブロッキング
     }
+
 }
 
